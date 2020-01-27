ee4b432996bed4a106aebbdd5c2f2a97e4f822e7
Vendela Eklund <wendelize@gmail.com>
2020-01-27 14:54:42 +0100

    Fill include

--- GameProject/GameProject/src/Header Files/Include.h
+++ GameProject/GameProject/src/Header Files/Include.h
@@ -1,1 +1,23 @@
 #pragma once
+//INCLUDES 3D
+#include <GL/glew.h>
+#include <glfw3.h>
+#include <glm.hpp>
+#include <gtc/type_ptr.hpp>
+
+//INCLUDES C++
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <math.h>
+
+//CLASSES
+#include "Header Files/Window.h"
+#include "Header Files/Shader.h"
+#include "Header Files/Camera.h"
+#include "Header Files/ObjectData.h"
+
+//NAMESPACES
+using namespace std;
+using namespace glm;
