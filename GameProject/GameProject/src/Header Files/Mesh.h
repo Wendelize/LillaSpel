2f68159be8f88b004959e4819f3544b7ec98a7da
Vendela Eklund <wendelize@gmail.com>
2020-01-28 15:11:17 +0100

    We have classes in m and m

--- GameProject/GameProject/src/Header Files/Mesh.h
+++ GameProject/GameProject/src/Header Files/Mesh.h
@@ -1,29 +1,20 @@
 #pragma once
-//INCLUDES 3D
-#include <GL/glew.h>
-#include <glfw3.h>
-#include <glm.hpp>
-#include <gtc/type_ptr.hpp>
-#include "assimp/scene.h"
-#include "assimp/postprocess.h"
-#include "assimp/Importer.hpp"
+
+class Mesh
+{
+private:
+
+public:
+	Mesh();
+	~Mesh();
 
 
-//INCLUDES C++
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
-#include <math.h>
+};
 
-//CLASSES
-#include "Window.h"
-#include "Shader.h"
-#include "Camera.h"
-#include "ObjectData.h" 
-#include "Platform.h"
-#include "Player.h"
+Mesh::Mesh()
+{
+}
 
-//NAMESPACES
-using namespace std;
-using namespace glm;
+Mesh::~Mesh()
+{
+}
--- GameProject/GameProject/src/Header Files/Model.h
+++ GameProject/GameProject/src/Header Files/Model.h
@@ -1,29 +1,19 @@
 #pragma once
-//INCLUDES 3D
-#include <GL/glew.h>
-#include <glfw3.h>
-#include <glm.hpp>
-#include <gtc/type_ptr.hpp>
-#include "assimp/scene.h"
-#include "assimp/postprocess.h"
-#include "assimp/Importer.hpp"
+class Model
+{
+private:
+
+public:
+	Model();
+	~Model();
 
 
-//INCLUDES C++
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
-#include <math.h>
+};
 
-//CLASSES
-#include "Window.h"
-#include "Shader.h"
-#include "Camera.h"
-#include "ObjectData.h" 
-#include "Platform.h"
-#include "Player.h"
+Model::Model()
+{
+}
 
-//NAMESPACES
-using namespace std;
-using namespace glm;
+Model::~Model()
+{
+}
