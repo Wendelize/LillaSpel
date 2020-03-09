23679ed920da2a8250943a2790ea45c481b93914
Björn <andromeda_stargate@hotmail.com>
2020-03-09 13:55:52 +0100

    Model och mesh bör vara klara för textures

--- GameProject/GameProject/src/Header Files/Model.h
+++ GameProject/GameProject/src/Header Files/Model.h
@@ -5,17 +5,19 @@ class Model
 {
 private:
 	vector<Mesh> m_meshes;
+	vector<TextureData> m_texturesLoaded; // stores all the textures loaded so far, optimization to make sure textures aren't loaded more than once.
+	bool m_textureBool = false;
 	string m_dir = "../Models/";
 
 	void LoadModel(string path);
 	void ProcessNode(aiNode* node, const aiScene* scene);
 	Mesh ProcessMesh(aiMesh* mesh, const aiScene* scene);
-	vector<TextureData> LoadMaterialTextures(aiMaterial* mat, aiTextureType type,
-		string typeName);
+	TextureData LoadTexture(aiMaterial* mat, aiTextureType type, string typeName);
 	Material loadMaterial(aiMaterial* mat);
+	int TextureFromFile(const char* path, const string& directory, bool gamma);
 
 public:
-	Model(const char * path);
+	Model(const char * path, bool textureBool = false);
 	~Model();
 
 	void Draw(Shader* shader);
